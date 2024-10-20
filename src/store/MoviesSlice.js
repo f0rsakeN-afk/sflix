3b793cf6558b0c2942392169a2fd632e963a072b
f0rsakeN-afk <nar3sh0@gmail.com>
2024-10-20 19:19:35 +0545

    fixed trending api malfunction

--- src/store/MoviesSlice.js
+++ src/store/MoviesSlice.js
@@ -13,26 +13,24 @@ const initialState = {
 };
 
 export const fetchMovies = createAsyncThunk(
-    'movies/fetch', 
-    async () => {
-      const options = {
-        method: 'GET',
-        url: 'https://api.themoviedb.org/3/trending/all/day',
-        params: { language: 'en-US' },
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiM2YxZDA1NzQ3MzQ5NTY2YjVhZjhlMmJlY2ExYTE3OCIsInN1YiI6IjY1ZmM1OWU2NjA2MjBhMDEzMDI1NTY0OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lw9nf130uEt4eiAXHM3H_qUc7u-aSQeRCHLVv1uEKH8'
-        }
-      };
-  
-      try {
-        const response = await axios.request(options);
-        return response.data;
-      } catch (error) {
-        throw error;
+  'movies/fetch',
+  async () => {
+    const options = {
+      method: 'GET',
+      url: 'https://api.themoviedb.org/3/trending/movie/day?language=en-US',
+      params: { language: 'en-US' },
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiM2YxZDA1NzQ3MzQ5NTY2YjVhZjhlMmJlY2ExYTE3OCIsInN1YiI6IjY1ZmM1OWU2NjA2MjBhMDEzMDI1NTY0OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lw9nf130uEt4eiAXHM3H_qUc7u-aSQeRCHLVv1uEKH8'
       }
-    }
-  );
+    };
+
+
+    const response = await axios.request(options);
+    return response.data;
+
+  }
+);
 
 const movieSlice = createSlice({
   name: "movies",
@@ -42,8 +40,8 @@ const movieSlice = createSlice({
       state.status = STATUSES.LOADING;
     });
     builder.addCase(fetchMovies.fulfilled, (state, action) => {
-        state.data = action.payload;
-        //console.log(action.payload)
+      state.data = action.payload;
+      //console.log(action.payload)
       state.status = STATUSES.IDLE;
     });
     builder.addCase(fetchMovies.rejected, (state, action) => {
